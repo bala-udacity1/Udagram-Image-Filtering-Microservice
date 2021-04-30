b2764fa9c5a7c83c55418ce6bb706453e24cf7b1
bala-udacity1 <81705082+bala-udacity1@users.noreply.github.com>
2021-04-30 03:13:44 +0100

    implemented filteredimage endpoint
    implemented filteredimage endpoint & added requires valid-url dependency

--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,8 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import {filterImageFromURL, deleteLocalFiles} from './util/util';
 
+var validUrl = require('valid-url');
+
 (async () => {
 
   // Init the Express application
@@ -13,24 +15,18 @@ import {filterImageFromURL, deleteLocalFiles} from './util/util';
   // Use the body parser middleware for post requests
   app.use(bodyParser.json());
 
-  // @TODO1 IMPLEMENT A RESTFUL ENDPOINT
-  // GET /filteredimage?image_url={{URL}}
-  // endpoint to filter an image from a public url.
-  // IT SHOULD
-  //    1
-  //    1. validate the image_url query
-  //    2. call filterImageFromURL(image_url) to filter the image
-  //    3. send the resulting file in the response
-  //    4. deletes any files on the server on finish of the response
-  // QUERY PARAMATERS
-  //    image_url: URL of a publicly accessible image
-  // RETURNS
-  //   the filtered image file [!!TIP res.sendFile(filteredpath); might be useful]
+  app.get( "/filteredimage", async ( req, res ) => {  
+    const { image_url: filteredImageUrl } = req.query;
+    if (!filteredImageUrl || !validUrl.isUri(filteredImageUrl)) {
+      return res.status(400).send({ auth: false, message: 'image url is empty or invalid' });
+    }
 
-  /**************************************************************************** */
+    const filteredPath = await filterImageFromURL(filteredImageUrl);
 
-  //! END @TODO1
-  
+    res.sendFile(filteredPath, {} , () => deleteLocalFiles([filteredPath]));
+  });
+
+  /**************************************************************************** */ 
   // Root Endpoint
   // Displays a simple message to the user
   app.get( "/", async ( req, res ) => {
